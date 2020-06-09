47e0001ae3b104972671d650e390529856f12b48
usac201212895 <66638123+usac201212895@users.noreply.github.com>
2020-06-09 12:24:27 -0600

    version 2

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -1,12 +1,15 @@
-numero = int(input("Ingresa un numero: "))
-
-while numero != 1:
-    if numero % 2 == 0:
-        print("%d," % (numero), end = "")
-        numero = numero / 2
+#creamos la variable secuencia con int para que el dato esperado sea un numero, se envia un mensaje al usuario pidiendo el numero N
+secuencia = int(input("Numero N: "))
+#se inicia el bucle general
+while secuencia != 1:
+    #si el numero de secuencia ingresado es par, se lo divide por dos
+    if secuencia % 2 == 0:
+        print("%d," % (secuencia), end = "")
+        secuencia = secuencia / 2
+        #si no cumple y es impar, se le multiplica tres y se le suma uno
     else:
-        print("%d," % (numero), end = "")
-        numero = (numero * 3) + 1
-
-    if numero == 1:
+        print("%d," % (secuencia), end = "")
+        secuencia = (secuencia * 3) + 1
+#la secuencia termina la llegar a 1
+    if secuencia == 1:
         print("1")
